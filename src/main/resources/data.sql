9d7407d25264eb7aaa824b25eedf8fd545d801c9
shafin-10 <shafinshahriar28@gmail.com>
2025-11-29 01:48:29 +0600

    added courses and classes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -32,3 +32,56 @@ VALUES('STUDENT', CURRENT_DATE, 'DBA')
 Insert INTO person(name,mobile_number,email,pwd,role_id,created_at, created_by)
 VALUES('Admin','01887757996','admin@gmail.com','123456',1,CURRENT_DATE,'DBA')
 
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Adam','2176436587','zadam@gmail.com','Regarding a job','Wanted to join as teacher','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Zara','3412654387','zarabaig@hotmail.com','Course Admission','Wanted to join a course','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Marques','8547643673','kmarques@yahoo.com','Course Review','Review of Development course','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Shyam','4365328776','gshyam@gmail.com','Admission Query','Need to talk about admission','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('John','5465765453','doejohn@gmail.com','Holiday Query','Query on upcoming holidays','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Taniya Bell','3987463827','belltaniya@gmail.com','Child Scholarship','Can my child get scholarship?','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Willie Lara','4568764801','476lara@gmail.com','Need Admission','My son need an admission','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Jonathan Parsons','4321768902','jonathan.parsons@gmail.com','Course feedback','Music course is good','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Cloe Rubio','9854438719','rubio987@gmail.com','Correct Date of Birth','My Child DOB needs to be corrected','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Camilla Stein','6545433254','camillas@gmail.com','Transport Query','Is Transport provided?','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Lizeth Gross','4678783434','grossliz@yahoo.com','Progress report','Please send progress report','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Yael Howe','1243563254','howeyael@gmail.com','Certificate Query','Need Certificate hard copy','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Ian Moreno','2312231223','moreno.ian@gmail.com','Food feedback','Food quality can be improved','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Desirae Ibarra','3445235667','ibarrades@gmail.com','Traffic Complaint','Traffic around school can be controlled','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Oswaldo Jarvis','4556121265','jarvissmile@hotmail.com','Study Tour','Study tour details needed','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Miah Perkins','2367784512','perkinsmiah@hotmail.com','Vaccination Support','Vaccination center in the school','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Zion Bolton','8990678900','boltzion@gmail.com','Course fees','Pls share fees of music course','Open', CURRENT_DATE,'DBA');
+
+INSERT INTO contact_msg (name, mobile_num, email, subject, message, status, created_at, created_by)
+VALUES ('Dominik Tanner','4556127834','tannerdominik@gmail.com','Games schedule','Provide Summer games schedule','Open', CURRENT_DATE,'DBA');
